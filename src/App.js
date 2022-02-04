00638614237ec02f6d886567a9ce87e793658161
Ron H <ronald.hirschauer@gmail.com>
2022-02-04 18:51:30 -0500

    Updating CRA file for current project.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './Components/Navbar';
+import Hero from './Components/Hero';
+import Card from './Components/Card';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      {/* <Hero /> */}
+      <Card 
+        img="katie-zaferes.png"
+        rating = "5.0"
+        reviewCount="(6)"
+        country="USA"
+        title="Life lessons with Katie Zaferes"
+        price="136"
+      />
     </div>
   );
 }
